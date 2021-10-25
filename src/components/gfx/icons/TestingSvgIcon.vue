8a241fde6ad80541d25ec2bbefef1c555e0f9c14
Marc <marcbaque1311@gmail.com>
2021-10-25 20:56:22 +0200

    [ ADD ] component tests for svg icon

---
+++ src/components/gfx/icons/TestingSvgIcon.vue
@@ -0,0 +1,39 @@
+<script setup lang='ts'>
+import { ref } from 'vue';
+import { ICON_SIZE } from './icon-data';
+import SvgIcon from './SvgIcon.vue';
+const iconColor = ref('rgb(48, 187, 78)')
+</script>
+
+<template lang='pug'>
+.testing-svg-icon
+  .row
+    SvgIcon(data-cy="size-xs" :size="ICON_SIZE.xs")
+    SvgIcon(data-cy="size-s" :size="ICON_SIZE.s")
+    SvgIcon(data-cy="size-m" :size="ICON_SIZE.m")
+    SvgIcon(data-cy="size-l" :size="ICON_SIZE.l")
+    SvgIcon(data-cy="size-xl" :size="ICON_SIZE.xl")
+  .row
+    SvgIcon(data-cy="color-red" color="red")
+    SvgIcon(data-cy="color-hex" color="#3c67d3")
+    SvgIcon(data-cy="color-var" :color="iconColor")
+  .row
+    SvgIcon(data-cy="house-user" icon-name="house-user")
+    SvgIcon(data-cy="laptop-code" icon-name="laptop-code")
+    SvgIcon(data-cy="sparkles" icon-name="sparkles")
+</template>
+
+<style lang='scss' scoped>
+.testing-svg-icon {
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  color: rgb(48, 187, 78);
+}
+
+.row {
+  display: flex;
+  gap: 1em;
+}
+</style>
