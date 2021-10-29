39ec4f9d9f37d9ac15b4049641d2f856671e5b43
Marc <marcbaque1311@gmail.com>
2021-10-30 00:39:32 +0200

    [ ADD ] method to determine the intrinsic ratio of the playing video

--- src/components/video/VideoPlayer.vue
+++ src/components/video/VideoPlayer.vue
@@ -1,13 +1,27 @@
 <script setup lang='ts'>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
-const props = withDefaults(defineProps<{ url: string, poster?: string, ratio: number }>(), { ratio: 9 / 16 })
-const getRatio = computed(() => `${props.ratio * 100}%`)
+const props = withDefaults(defineProps<{ url: string, poster?: string, ratio?: number }>(), { ratio: 9 / 16 });
+const getRatio = computed(() => `${props.ratio * 100}%`);
+const intrinsicRatio = ref<null | string>(null);
+
+function onPlay(event: Event) {
+  if (intrinsicRatio.value != null) return;
+
+  setTimeout(() => {
+    const { height, width } = (event.target as HTMLVideoElement).getBoundingClientRect();
+    const ratioInstrinsic = `${height / width * 100}%`;
+
+    if (ratioInstrinsic !== getRatio.value) {
+      intrinsicRatio.value = ratioInstrinsic;
+    }
+  }, 50)
+}
 </script>
 
 <template lang='pug'>
-.video-player(:style="{ paddingTop: getRatio }")
-  video(controls :poster="props.poster")
+.video-player(:style="{ paddingTop: intrinsicRatio ?? getRatio }")
+  video(controls :poster="props.poster" @play="onPlay")
     source(:src="props.url")
 </template>
 
