d9e5831a65c02635c55c3b763ab55d195eb6fadb
Marc <marcbaque1311@gmail.com>
2021-11-02 23:49:09 +0100

    [ ADD ] initial responsive styling for UserAvatarEditor

--- src/components/profile/UserAvatarEditor.vue
+++ src/components/profile/UserAvatarEditor.vue
@@ -27,47 +27,83 @@ function onDeselectClick() {
 <template lang='pug'>
 .user-avatar-editor
   img.user-avatar(data-cy="user-avatar" :src="authStore.getAvatarUrl")
-  Transition(name="fade")
-    .file-picker(v-if="!previewImageLoaded")
-      label.btn.btn_primary.btn_file-picker(
-        for="new-avatar"
-        data-cy="new-avatar-label"
-      ) Neues Avatarbild auswählen...
-      input#new-avatar(
-        type="file"
-        accept="image/jpeg"
-        name="new-avatar"
-        data-cy="input-new-avatar"
-        @change="onFileChange"
-      )
-    button.btn.btn_cancel.file-deselect(
-      v-else
-      @click="onDeselectClick"
-      data-cy="preview-avatar-deselect"
-    ) Rückgängig
+  .file-picker(v-if="!previewImageLoaded")
+    label.btn.btn_primary.btn_file-picker(
+      for="new-avatar"
+      data-cy="new-avatar-label"
+    ) Neues Avatarbild auswählen...
+    input#new-avatar(
+      type="file"
+      accept="image/jpeg"
+      name="new-avatar"
+      data-cy="input-new-avatar"
+      @change="onFileChange"
+    )
   Transition(name="fade")
     .new-avatar__preview(v-show="previewImageLoaded")
-      p(data-cy="preview-label") Vorschau
+      h3.preview-label(data-cy="preview-label") Vorschau
       img.preview-image.user-avatar(
         ref="previewImageEl"
         data-cy="preview-image"
       )
-      button.btn.btn_primary.btn_upload(data-cy="button-upload") Hochladen
+      .actions
+        button.btn.btn_cancel.file-deselect(
+          v-if="previewImageLoaded"
+          @click="onDeselectClick"
+          data-cy="preview-avatar-deselect"
+        ) Rückgängig
+        button.btn.btn_primary.btn_upload(data-cy="button-upload") Hochladen
 </template>
 
 <style lang='scss' scoped>
+@use '../../styles/mixins' as *;
+
+.user-avatar-editor {
+  margin-bottom: 2em;
+}
+
 .user-avatar {
   width: 64px;
   border-radius: 50%;
   overflow: hidden;
-  margin-bottom: 1em;
+  margin-bottom: 2em;
 }
 
 .btn_file-picker {
   cursor: pointer;
+  display: inline-block;
+  width: 100%;
+  text-align: center;
+
+  @include mq("p-l") {
+    width: max-content;
+  }
 }
 
 #new-avatar {
   display: none;
+}
+
+.preview-label {
+  margin-bottom: 0.5em;
+  font-weight: 500;
+}
+
+.actions {
+  display: flex;
+  justify-content: space-between;
+  gap: 1em;
+
+  button {
+    flex: 1;
+  }
+
+  @include mq("t-p") {
+    justify-content: revert;
+
+    button {
+      flex: revert;
+    }
+  }
 }
 </style>
