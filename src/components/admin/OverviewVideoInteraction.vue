0493e7d0f42beff8f6cb97ea1e5e0d0300689f28
Marc <marcbaque1311@gmail.com>
2021-11-16 22:43:05 +0100

    [ UPDATE ] improve video approval

--- src/components/admin/OverviewVideoInteraction.vue
+++ src/components/admin/OverviewVideoInteraction.vue
@@ -3,22 +3,29 @@ import { ref } from 'vue';
 import { useVideoStore } from '../../stores/video';
 import type { Video } from '../../types/models/video';
 import KvCheckbox from '../forms/elements/KvCheckbox.vue';
+import LoaderIndeterminate from '../gfx/loaders/LoaderIndeterminate.vue';
 
 const videoStore = useVideoStore();
 
 defineProps<{ video: Video, type: 'approve' | 'not-listed' | 'listed' }>();
 const emits = defineEmits(['reload:overiew'])
 
+const isLoading = ref(false);
 const listVideo = ref(true);
 
 async function onApproveClick(videoId: string) {
+  if (isLoading.value) return;
+
+  isLoading.value = true
   await videoStore.approveVideo({ videoId, listVideo: listVideo.value });
   emits('reload:overiew')
+  isLoading.value = false;
 }
 </script>
 
 <template lang='pug'>
 article.overview-video-interaction
+  LoaderIndeterminate(:class="{ visible: isLoading }")
   section.meta-data
     h3.title
       a(:href="video.url" target="_blank" rel="noopener")
@@ -27,26 +34,46 @@ article.overview-video-interaction
     p.description {{ video.description }}
     .actions(v-if="type === 'approve'")
       .negavive
-        button.btn.btn_cancel(type="button") Video löschen
+        button.btn.btn_cancel(
+          type="button"
+          :disabled="isLoading"
+        ) Video löschen
       .positive
         KvCheckbox(:name="video.hash" v-model:checked="listVideo") nach Freigabe listen?
-        button.btn.btn_primary(type="button" @click="onApproveClick(video._id)") Video freigeben
+        button.btn.btn_primary(
+          type="button"
+          :disabled="isLoading"
+          @click="onApproveClick(video._id)"
+        ) Video freigeben
 
     .actions(v-if="type === 'not-listed'")
       .negavive
-        button.btn.btn_cancel(type="button") Video löschen
+        button.btn.btn_cancel(
+          type="button"
+          :disabled="isLoading"
+        ) Video löschen
       .positive
-        button.btn.btn_primary(type="button") Video listen
+        button.btn.btn_primary(
+          type="button"
+          :disabled="isLoading"
+        ) Video listen
 
     .actions(v-if="type === 'listed'")
       .negavive
-        button.btn.btn_cancel(type="button") Video löschen
+        button.btn.btn_cancel(
+          type="button"
+          :disabled="isLoading"
+        ) Video löschen
       .positive
-        button.btn.btn_primary(type="button") Video nicht mehr listen
+        button.btn.btn_primary(
+          type="button"
+          :disabled="isLoading"
+        ) Video nicht mehr listen
 </template>
 
 <style lang='scss' scoped>
 .overview-video-interaction {
+  position: relative;
   gap: 1em;
   background-color: var(--color-ui);
 }
