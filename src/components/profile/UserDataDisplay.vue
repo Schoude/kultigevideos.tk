4b9b5221eccb6644d9df3e8f05dd680d19157c57
Marc <marcbaque1311@gmail.com>
2021-11-18 23:27:05 +0100

    [ ADD ] user details display for user profile

--- src/components/profile/UserDataDisplay.vue
+++ src/components/profile/UserDataDisplay.vue
@@ -1,6 +1,7 @@
 <script setup lang='ts'>
 import { useRouter } from 'vue-router';
 import { useUserStore } from '../../stores/user';
+import UserDetails from './UserDetails.vue';
 
 const router = useRouter();
 const userStore = useUserStore();
@@ -10,8 +11,12 @@ await userStore.fetchUserProfileData(router.currentRoute.value.params.id as stri
 
 <template lang='pug'>
 section.user-data-display
-  | {{ userStore.getUserProfileData }}
+  UserDetails
 </template>
 
 <style lang='scss' scoped>
+.user-data-display {
+  max-width: 1000px;
+  margin: 0 auto;
+}
 </style>
---
+++ src/components/profile/UserDetails.vue
@@ -0,0 +1,85 @@
+<script setup lang='ts'>
+import { computed } from '@vue/reactivity';
+import { usePageHelpers } from '../../composables/page-helpers';
+import { useUserStore } from '../../stores/user';
+
+const userStore = useUserStore();
+const { createFormattedDurationStringFromSeconds } = usePageHelpers();
+
+const getTotalDurationString = computed(() => {
+  const time = createFormattedDurationStringFromSeconds(userStore.getUserProfileData?.totalVideoDuration as number);
+
+  return userStore.getUserProfileData?.totalVideoDuration as number > 3600
+    ? `${time} Stunden`
+    : userStore.getUserProfileData?.totalVideoDuration as number > 60
+      ? `${time} Minuten` : `${time} Sekunden`
+}
+);
+</script>
+
+<template lang='pug'>
+section.user-details
+  article.user
+    img.avatar(
+      :alt="`Avatar pictur of ${userStore.getUserProfileData?.username}`"
+      :src="userStore.getUserProfileData?.meta.avatarUrl"
+    )
+    h1.headline {{ userStore.getUserProfileData?.username }}
+
+  article.user-performance
+    h2.headline Performance-Daten
+    section.performance-data
+      p.total-views Anzahl Aufrufe: <br> {{ userStore.getUserProfileData?.totalViewcount }}
+      p.total-duration Gesamtlaufzeit der Videos: <br> {{ getTotalDurationString }}
+      p.total-likes Anzahl Likes: <br> {{ userStore.getUserProfileData?.totalLikes }}
+      p.total-dislikes Anzahl Dislikes: <br> {{ userStore.getUserProfileData?.totalDislikes }}
+
+</template>
+
+<style lang='scss' scoped>
+@use '../../styles/mixins' as *;
+
+article {
+  border-bottom: 1px solid var(--border-color-container);
+  padding-bottom: 2em;
+
+  & + & {
+    margin-top: 2em;
+  }
+}
+
+.user {
+  display: flex;
+  align-items: center;
+}
+
+.avatar {
+  width: 100px;
+}
+
+.headline {
+  font-size: 24px;
+}
+
+.user {
+  .headline {
+    font-size: 26px;
+    margin-left: 2em;
+  }
+}
+
+.user-performance {
+  .headline {
+    margin-bottom: 0.25em;
+  }
+}
+
+.performance-data {
+  display: grid;
+  row-gap: 1em;
+
+  @include mq("t-p") {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+</style>
