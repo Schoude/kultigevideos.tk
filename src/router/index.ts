1b6e59b19be0e9c6489f99bb65180b6be7fde2d5
Marc <marcbaque1311@gmail.com>
2021-11-21 16:02:04 +0100

    [ UPDATE ] router scrollBehaviour

--- src/router/index.ts
+++ src/router/index.ts
@@ -140,6 +140,9 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    return { top: 0 };
+  },
 });
 
 router.beforeEach(async (to, from, next) => {
